86d3abb5cd1b0b30b65d265bcdfe90ee3c17fdc9
Renatebh <renate.hem@gmail.com>
2021-12-07 10:45:23 +0000

    added a js file + changese in html

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+function changeBackground(color) {
+  document.getElementById("container").style.background = color;
+}
+
+window.addEventListener("load", function () {
+  changeBackground("#DDA0DD");
+});
+
